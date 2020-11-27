1a44c3c73f8715de9c7758e2c2810f69bd8608bb
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2020-11-27 10:28:31 +0100

    Pruebas de comprobacion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,23 @@ public class Main {
         //intento instanciar un objeto con el constructor privado
           Singleton miUnicaInstancia = new Singleton();
 
+        // esto es un error
+        // Singleton miUnicaInstancia = new Singleton();
+
+        // creamos un objeto
+        Singleton miUnicaInstancia = Singleton.getInstance();
+        // rellenamos los atributos
+        miUnicaInstancia.setEdad(20);
+        miUnicaInstancia.setNombre("Pepe");
+
+        // creamos otro objeto Singleton
+        // como la única manera es con getInstance(), este método se encargará
+        // de devolvernos el objeto creado anteriormente
+        Singleton otraInstanciaSeraLaMisma = Singleton.getInstance();
+        // podemos cambiar la edad
+        // comprobar con el debug que los dos objetos:
+        // miUnicaInstancia y otraInstanciaSeraLaMisma son el mismo
+        otraInstanciaSeraLaMisma.setEdad(34);
+
     }
 }
